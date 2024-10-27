98bcc97b4a8183f0431d4290fbc2a3aec6756fc2
Ruth Melese <rmelese1@gmail.com>
2024-10-27 14:04:57 -0400

    Added new message

--- app/main.py
+++ app/main.py
@@ -10,8 +10,13 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Hello": "Welcome to My API"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/info")
+def info():
+    return {"data": "This is some additional information"}
+
